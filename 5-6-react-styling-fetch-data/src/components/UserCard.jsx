fe03c313fc5dc4369c95ee4b1fce536839ecbfb0
Yasmeen Alshehri <s202271660@kfupm.edu.sa>
2026-03-04 11:40:25 +0300

    TODO 3.1

--- 5-6-react-styling-fetch-data/src/components/UserCard.jsx
+++ 5-6-react-styling-fetch-data/src/components/UserCard.jsx
@@ -18,6 +18,8 @@ function UserCard({ user, onUserClick }) {
 
         {/* TODO 3.1: Add ONE <Button> here (text: "View Details") */}
         {/* TODO 3.1: onClick MUST call onUserClick and pass the current user object */}
+
+        <Button onClick={() => onUserClick(user)}>View Details</Button>
       </Card.Body>
     </Card>
   );
