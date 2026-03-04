0b636e52a2006a2dacc9bc36f04305d16fda856a
Yasmeen Alshehri <s202271660@kfupm.edu.sa>
2026-03-04 11:29:53 +0300

    TODO 2.1

--- 5-6-react-styling-fetch-data/src/App.jsx
+++ 5-6-react-styling-fetch-data/src/App.jsx
@@ -204,6 +204,31 @@ export default function App() {
      ========================================================= */
   useEffect(() => {
     // TODO 2.1: Implement fetching users here (see lab instructions)
+
+        const fetchUsers = async () => {
+      try {
+        setLoading(true);
+        setError(null);
+
+        const response = await fetch("https://jsonplaceholder.typicode.com/users");
+
+        // If API fails throw an Error to be catched cleanly
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch users");
+        }
+
+        const data = await response.json();
+        setUsers(data);
+        setFilteredUsers(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUsers();
   }, []);
 
   /* =========================================================
